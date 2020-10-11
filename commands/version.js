c5107281f310d55a9ff1f474c5e50527a6b6aa5d
miachal <61947247+miachal@users.noreply.github.com>
2020-10-11 22:09:37 +0200

    embed as version

--- commands/version.js
+++ commands/version.js
@@ -1,9 +1,14 @@
+const { MessageEmbed } = require('discord.js');
+
 const { version } = require('../config.json');
 
 module.exports = {
   cmd: 'version',
   description: 'Shows actual version.',
   run: ({ msg }) => {
-    msg.channel.send('Version: ', version);
+    const embed = new MessageEmbed().setColor(0x00ff00).setTitle('♪ discobot');
+    embed.addField('Version', version);
+    embed.addField('Github', 'github.com/miachal/discobot');
+    msg.channel.send(embed);
   },
 };
