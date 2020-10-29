358f657a758ee3c0be047587f9db32c7dfa5bc60
miachal <61947247+miachal@users.noreply.github.com>
2020-10-29 17:42:57 +0100

    we need more dzbans!

--- commands/dzban.js
+++ commands/dzban.js
@@ -4,9 +4,12 @@ module.exports = {
   cmd: 'dzban',
   description: 'Super secret functionality, use it at your own risk.',
   run: ({ msg }) => {
-    const dzban = new MessageAttachment(
-      'https://media.discordapp.net/attachments/714489357964410930/764785697433321502/dzbank.png?width=1080&height=608'
+    const dzban = [
+      'https://media.discordapp.net/attachments/714489357964410930/764785697433321502/dzbank.png',
+      'https://cdn.discordapp.com/attachments/714489357964410930/770762543030861854/dzban_2.0.png',
+    ];
+    msg.channel.send(
+      new MessageAttachment(dzban[(Math.random() * dzban.length) | 0])
     );
-    msg.channel.send(dzban);
   },
 };
